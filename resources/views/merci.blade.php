425332cffa56337d76e035c0016ed67d93f82b04
gitabe <becqueta@gmail.com>
2015-11-21 16:37:34 +0100

    maj template

--- resources/views/merci.blade.php
+++ resources/views/merci.blade.php
@@ -5,8 +5,8 @@
         <div class="container">
             <div class="intro-text">
                 <div class="intro-lead-in"></div>
-                <div class="intro-heading" style="background-color:rgba(0,0,0,0.3)">Merci d'avoir signaler l'incident</div>
-                <a href="#services" class="page-scroll btn btn-xl">Tell Me More</a>
+                <div class="intro-heading">Merci d'avoir signaler l'incident</div>
+                <a href="#services" class="page-scroll btn btn-xl">We are coming soon</a>
             </div>
         </div>
     </header>
