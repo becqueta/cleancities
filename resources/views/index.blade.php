70c5c3ef970b9cb9b921a95a4fb4807e3460a215
gitabe <becqueta@gmail.com>
2015-11-22 09:57:05 +0100

    ioubfezcdv

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
             <div class="intro-text">
                 <div class="intro-lead-in"></div>
                 <div class="intro-heading">Tu veux rendre ta ville belle et propre?</div>
-                <a href="#services" class="page-scroll btn btn-xl">Télécharge l'application <i class="fa fa-download"></i></a>
+                <a href="#services" class="page-scroll btn btn-xl">Télécharge<br> l'application <i class="fa fa-download"></i></a>
             </div>
         </div>
     </header>
